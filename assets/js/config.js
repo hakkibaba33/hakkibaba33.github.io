4fe7f3d5c51dcfc2fa9614ea94e1a1431681f44f
hakkibaba33 <hakkiislek333@gmail.com>
2026-07-07 18:22:34 +0200

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+    SUPABASE: {
+        URL: 'https://mowyhsssmpjrwxzvelax.supabase.co',
+        ANON_KEY: 'sb_publishable_Fp4Nstc2lpCPNL-5OHPU8w_KD0AC6xf'
+    },
+    STRIPE: {
+        PUBLISHABLE_KEY: '',
+        SUCCESS_URL: 'https://dikrug.se/tack',
+        CANCEL_URL: 'https://dikrug.se/kassa'
+    }
+};
