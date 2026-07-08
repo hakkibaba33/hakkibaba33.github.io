2f20db11893b1f5555ac9f712de9356c10781559
hakkibaba33 <hakkiislek333@gmail.com>
2026-07-08 02:30:23 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
         ANON_KEY: 'sb_publishable_Fp4Nstc2lpCPNL-5OHPU8w_KD0AC6xf'
     },
     STRIPE: {
-        PUBLISHABLE_KEY: '',
+        PUBLISHABLE_KEY: 'pk_test_51TiZYcBSwC91LA4pGzXdLDpfdgM78kznpXb5U7hedy3ox6EgLbHM5L7bXpTKmHmiUwu6JkhKZaX0EH3sZREiXXDt00a3qyz6eL',
         SUCCESS_URL: 'https://dikrug.se/tack',
         CANCEL_URL: 'https://dikrug.se/kassa'
     }
